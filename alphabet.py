bbc735564ad555df5e1b909929358fd1ba84401a
Dharshine Palanisamy <43207072+Dharu-P@users.noreply.github.com>
2019-06-28 13:04:01 +0530

    Alphabet or not

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+a=input()
+alpha=['a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if(a in alpha):
+    print("Alphabet")
+else:
+    print("No")
