c7f46f8fec04be1a3f9cfa8d9b918a7842941a8b
Dharshine Palanisamy <43207072+Dharu-P@users.noreply.github.com>
2019-06-28 13:00:26 +0530

    vowel.py
    check whether the given character is vowel or character

---
+++ vowels.py
@@ -0,0 +1,9 @@
+a=input()
+vowels=['a','e','i','o','u']
+consonant=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if(a in vowels):
+    print("Vowel")
+elif(a in consonant):
+    print("Consonant")
+else:
+    print("Invalid")
